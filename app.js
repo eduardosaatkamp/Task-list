3f2e3fbc457677297f896e91266bde62fc2d450e
Eduardo Saatkamp <eduardosaatkamp@gmail.com>
2023-06-16 04:31:52 +0000

    Atualizado tema einserido  data atual

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-'use strict'
-const switcher=document.querySelector('.btn');
+'use strict';
 
-switcher.addEventListener('click',function(){
-    document.body.classList.toggle('dark-theme');
+// Seleciona o elemento com a classe 'btn'
+const switcher = document.querySelector('.btn');
 
-/* precisa retornar depois para dark */
-var ClassName = document.body.className; 
-var icone = document.getElementById("icone");
+// Adiciona um ouvinte de evento de clique ao elemento 'switcher'
+switcher.addEventListener('click', function() {
+  // Toggla a classe 'dark-theme' no elemento 'body'
+  document.body.classList.toggle('dark-theme');
 
-if(ClassName == 'light-theme')
-{icone.style.color = '#0400fb';} 
-else{icone.style.color = '#fae900';}
- console.log('current class name:'+ClassName);
+  // Obtém a classe atual do elemento 'body'
+  var className = document.body.className;
+  var icone = document.getElementById("icone");
+
+  // Verifica se a classe é 'dark-theme' e define a cor do ícone de acordo
+  if (className == 'dark-theme') {
+    icone.style.color = '#fae900';
+  } else {
+    icone.style.color = '#0400fb';
+  }
+
+  // Imprime a classe atual no console
+  console.log('current class name: ' + className);
 });
+
+// Obtém o elemento <span> pelo ID
+const dataElement = document.getElementById('data-atual');
+
+// Obtém a data atual
+const dataAtual = new Date();
+
+// Formata a data para o formato desejado
+const options = { day: 'numeric', month: 'long', year: 'numeric' };
+const dataFormatada = dataAtual.toLocaleDateString('pt-BR', options);
+
+// Define o texto do elemento <span> para exibir a data atual completa
+dataElement.textContent = dataFormatada;
